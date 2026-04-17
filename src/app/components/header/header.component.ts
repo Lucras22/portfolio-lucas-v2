76c9ee2c963a7ac1172b6501a218026b9cfa1f46
Lucras Galindo <devlucasgalindo@gmail.com>
2026-04-17 17:52:47 -0300

    feat: implementar componente header com menu responsivo e signals

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,22 @@
+import { Component, signal} from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+  // Estado do menu usando Signals (Angular 19)
+  isMenuOpen = signal(false);
+
+  toggleMenu() {
+    this.isMenuOpen.update(value => !value);
+  }
+
+  closeMenu() {
+    this.isMenuOpen.set(false);
+  }
+}
