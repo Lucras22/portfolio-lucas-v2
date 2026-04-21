0b0bf15dc08f1fa715fad32302c52a96869bb7c6
Lucras Galindo <devlucasgalindo@gmail.com>
2026-04-21 18:43:44 -0300

    feat: atualizar identidade visual no footer e componente Genesis HiTech

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
-
+  // Você pode adicionar um sinal para o ano atual se quiser ser ultra preciso
+  currentYear = new Date().getFullYear();
 }
--- src/app/components/genesis-hitech/genesis-hitech.component.ts
+++ src/app/components/genesis-hitech/genesis-hitech.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { fadeInAnimation } from '../../animations';
 
 @Component({
   selector: 'app-genesis-hitech',
   imports: [],
   templateUrl: './genesis-hitech.component.html',
-  styleUrl: './genesis-hitech.component.scss'
+  styleUrl: './genesis-hitech.component.scss',
+  animations: [fadeInAnimation]
 })
 export class GenesisHitechComponent {
 
